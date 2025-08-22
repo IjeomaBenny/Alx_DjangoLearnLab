67c36ba6066ed0c367f94ffe2b396acc94898dfb
ijeomaBenny <ijesmart40@gmail.com>
2025-08-22 13:09:50 +0100

    made adjustment to the  authentication URLs for login, logout, and registration

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from .views import list_books, list_authors, list_libraries, LibraryDetailView
 
-from .views import login_view, logout_view, register_view
+from . import views
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     path("books/", list_books, name="list_books"),
@@ -12,9 +13,10 @@ urlpatterns = [
 
 
 
-urlpatterns += [
-    path("login/", login_view, name="login"),
-    path("logout/", logout_view, name="logout"),
-    path("register/", register_view, name="register"),
+
+urlpatterns = [
+    path('register/', views.register_view, name='register'),
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
 ]
 
