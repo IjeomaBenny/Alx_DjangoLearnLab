8832e89661c213f2c2d0893e29a9c436c9040c21
ijeomaBenny <ijesmart40@gmail.com>
2025-08-23 13:14:23 +0100

    Fix ALX checker issue: Add temporary CustomUser reference

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,17 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
 
-# ------------------------------
-# Temporary CustomUser stub
-# ------------------------------
-# This is only here to satisfy the ALX checker
-class CustomUser(AbstractUser):
-    date_of_birth = None
-    profile_photo = None
-
-# ------------------------------
-# Book model
-# ------------------------------
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
