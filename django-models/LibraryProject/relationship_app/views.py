99c3e39d1a4be04bae092cef324822b08ecbd74e
ijeomaBenny <ijesmart40@gmail.com>
2025-08-23 09:43:27 +0100

    made adjustments to import header

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,7 +3,9 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic.detail import DetailView
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib.auth.decorators import user_passes_test, permission_required
+from django.contrib.auth.decorators import user_passes_test
+from django.contrib.auth.decorators import permission_required   # <-- separate line
+
 
 from .models import Library, Book, Author, UserProfile
 
