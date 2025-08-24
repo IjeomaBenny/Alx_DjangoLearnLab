3b4847fbe3074204e0a16ceb80d8907cc933717c
ijeomaBenny <ijesmart40@gmail.com>
2025-08-24 14:24:56 +0100

    Add BookViewSet with CRUD endpoints and router configuration

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,6 +1,17 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from .views import BookList
+from .views import BookViewSet
+
+router = DefaultRouter()
+router.register(r'books_all', BookViewSet, basename='book_all')
 
 urlpatterns = [
-    path('books/', BookList.as_view(), name='book-list'),
+    path('books/', BookList.as_view(), name='book-list'), # existing list view
+    path('', include(router.urls)),  # all CRUD operations via the router
 ]
+
+
+
+
+
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 
+from rest_framework import viewsets
 from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
@@ -7,6 +8,12 @@ from .serializers import BookSerializer
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
 
 
 # Create your views here.
