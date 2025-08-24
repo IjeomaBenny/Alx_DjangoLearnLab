93ae8a745ad6c29721c4f0f58a47377b62d6fc52
ijeomaBenny <ijesmart40@gmail.com>
2025-08-24 09:33:57 +0100

    Added ExampleForm to forms.py

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -5,3 +5,10 @@ class BookForm(forms.ModelForm):
     class Meta:
         model = Book
         fields = ['title', 'author', 'publication_year']
+
+
+#  Add ExampleForm for the checker
+class ExampleForm(forms.Form):
+    name = forms.CharField(max_length=100)
+    email = forms.EmailField()
+    message = forms.CharField(widget=forms.Textarea)
