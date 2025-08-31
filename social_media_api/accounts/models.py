f801679b13d7e5febdc77e5593919ab940b2645c
ijeomaBenny <ijesmart40@gmail.com>
2025-08-31 12:35:58 +0100

    Social Media API: custom user model, auth, profile endpoints

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -7,7 +7,7 @@ from django.db import models
 # accounts/models.py
 class User(AbstractUser):
     bio = models.TextField(blank=True)
-    profile_picture = models.URLField(blank=True, null=True)  # safe replacement
+    profile_picture = models.ImageField(upload_to='profiles/', blank=True, null=True)
     followers = models.ManyToManyField(
         'self',
         symmetrical=False,
--- social_media_api/social_media_api/urls.py
+++ social_media_api/social_media_api/urls.py
@@ -21,7 +21,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('admin/', admin.site.urls),
     path('api/accounts/', include('accounts.urls')),
 ]
 
