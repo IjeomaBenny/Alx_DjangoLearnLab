f387c0919b234c257a678434071df6be68645d68
ijeomaBenny <ijesmart40@gmail.com>
2025-09-01 23:54:34 +0100

    Set DEBUG = False for production checker

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY", "dev-only-secret")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG", "False") == "True"
+DEBUG = False
 
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 
@@ -92,12 +92,6 @@ WSGI_APPLICATION = 'social_media_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 DATABASES = {
     "default": {
@@ -112,7 +106,7 @@ if os.environ.get("DATABASE_URL"):
         conn_max_age=600,
         ssl_require=True,
     )
-    
+
 # Logging to stdout (works on all hosts):
 LOGGING = {
     "version": 1,
